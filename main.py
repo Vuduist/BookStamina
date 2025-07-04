4ffceb213b7e6940072fe26616c40cbd658dc697
Vuduist <66877692+Vuduist@users.noreply.github.com>
2025-07-04 15:11:12 +0300

    Add more functions
    Я хз, делаю коммит через вебформу, боюсь, что всё наебнётся.

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from modules.ui import run_app
+import sys
+import sqlite3
+
+def clear_all():
+    # Очищаем таблицы в appdata.db
+    conn = sqlite3.connect('appdata.db')
+    c = conn.cursor()
+    c.execute('DELETE FROM history')
+    c.execute('DELETE FROM recent_books')
+    conn.commit()
+    conn.close()
+    # Очищаем таблицу в progress.db
+    conn = sqlite3.connect('progress.db')
+    c = conn.cursor()
+    c.execute('DELETE FROM progress')
+    conn.commit()
+    conn.close()
+    print('Все данные приложения успешно очищены.')
 
 if __name__ == "__main__":
-    run_app() 
+    if '--clear' in sys.argv:
+        clear_all()
+    else:
+        run_app() 
---
+++ readme.md
@@ -0,0 +1,5 @@
+Установка:
+pip install -r requirements.txt
+
+Запуск:
+python3 main.py
